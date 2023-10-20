3b614a83e7b8067f0210435b3e5260278abceb8a
LCastel <louis.castel34@icloud.com>
2023-10-21 01:15:08 +0200

    feat(accordion): animations gsap accordion v2 --wip

--- pages/hermes-ah24-accordion-2/hermes-ah24-accordion-2.js
+++ pages/hermes-ah24-accordion-2/hermes-ah24-accordion-2.js
@@ -1,40 +1,101 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll(".container .card img");
+  const cards = document.querySelectorAll(".card");
+  const caches = document.querySelectorAll(".cache");
+const btnClose = document.querySelector(".modal-close button")
+  cards.forEach((card) => {
+    card.classList.add("init");
+  });
+  images.forEach((img, key) => {
+    img.src = `https://source.unsplash.com/random?sig=${key}`;
+  });
 
+  caches.forEach((cache) => {
+    cache.addEventListener("click", () => {
+      const img = cache.previousElementSibling;
 
+      setTimeout(() => {
+        img.style.position = "absolute";
+        img.style.zIndex = "99";
+      }, 500);
 
-window.addEventListener('DOMContentLoaded', ()=>{
-    const images = document.querySelectorAll(".container .card img")
+      const rect = img.getBoundingClientRect();
+      const positionX = rect.left;
+      const positionY = rect.top;
 
-    images.forEach((img, key) =>{
-        img.src =`https://source.unsplash.com/random?sig=${key}`;
+      const cardFocus = img.closest(".card");
+      const cardAttribute = cardFocus.dataset.card;
+      console.log(cardAttribute);
+
+      cards.forEach((card) => {
+        cardFocus.style.height = "70vh";
+        cardFocus.style.flex = "1.8";
+        img.style.transform = "scale(1)";
+
+        card.classList.remove("init");
+
+        if (card !== cardFocus) {
+          card.classList.add("hidden");
+        }
+      });
+
+      const hiddenCards = document.querySelectorAll(".card.hidden");
+      const fondBlanc = document.querySelector(".fond-blanc");
+      const lineUp = document.querySelectorAll(".line-up");
+
+      lineUp.forEach((line) => {
+        if (cardAttribute === line.dataset.line) {
+          console.log("ici");
+          line.classList.add("focus");
+        }
+      });
+      const lineUpFocus = document.querySelector(".line-up.focus");
+      const tl = gsap.timeline();
+      tl.to(hiddenCards, {
+        opacity: 0,
+        duration: 0.3,
+        stagger: { amount: 0.1 },
+      });
+      tl.to(img, {
+        delay: 1.3,
+        left: `-${positionX}px`,
+        top: `-${positionY}px`,
+        delay: 0.2,
+        width: "100vw",
+        height: "100vh",
+      });
+      tl.to(
+        fondBlanc,
+        {
+          opacity: 1,
+          duration: 0.5,
+        },
+        "=-.3"
+      );
+      tl.to(
+        lineUpFocus,
+        {
+          opacity: 1,
+          duration: .5,
+        },
+        "=-.3"
+      );
+
+      
+      btnClose.addEventListener('click', () => {
+ 
+        tl.reverse();
+
+        cards.forEach((card) => {
+            card.classList.remove("hidden");
+            card.classList.add('init')
+        });
+        setTimeout(() => {
+      
+            cardFocus.removeAttribute('style')
+            img.removeAttribute('style');
+        },1370)
     })
-    console.log(images);
-    images.forEach((img) => {
-        console.log(img);
-        img.addEventListener('click', () => {
-            console.log('coucou');
-            const card = img.closest('.card');
-            console.log(card);
-            img.style.position = "absolute";
-            
-            const viewY = window.innerHeight;
-            const viewX = window.innerWidth;
-
-            const rect = img.getBoundingClientRect();
-
-            // Les propriétés rect.left et rect.top contiennent la position par rapport au viewport
-            const positionX = rect.left;
-            const positionY = rect.top;
-            img.style.zIndex = "999"
-        
-            console.log("viewport", viewX, viewY);
-            console.log('element', positionX,positionY)
-          
-            gsap.to(img,{
-              left:`-${positionX}px`,
-              top:`-${positionY}px`, 
-              width:"100vw",
-              height:"100vh",
-            })
-        })
-    })
-})
+    });
+  });
+});
