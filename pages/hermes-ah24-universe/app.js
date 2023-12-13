9cdb91cc86b4b8df8aaa814422507250544a0031
Louis <louis@glanum.com>
2023-12-13 09:05:01 +0100

    feat(paph-ah24): home

--- pages/hermes-ah24-universe/app.js
+++ pages/hermes-ah24-universe/app.js
@@ -1,24 +1,96 @@
+const arrStyle = {
+  all: [
+    [1, 1, 3, 2],
+    [1, 2, 2, 3],
+    [2, 2, 3, 3],
+    [1, 3, 3, 4],
+    [1, 4, 2, 5],
+    [2, 4, 3, 5],
+    [1, 5, 3, 6],
+    [3, 1, 4, 2],
+    [4, 1, 5, 2],
+    [3, 2, 5, 3],
+    [3, 3, 4, 4],
+    [4, 3, 5, 4],
+    [3, 4, 5, 5],
+    [3, 5, 4, 6],
+    [4, 5, 5, 6],
+  ],
+  clothes: [
+    [1, 1, 3, 2],
+    [1, 2, 3, 3],
+    [1, 3, 3, 4],
+    [1, 4, 3, 5],
+    [1, 5, 3, 6],
+    [3, 1, 5, 2],
+    [3, 2, 5, 3],
+    [3, 3, 5, 4],
+    [3, 4, 5, 5],
+    [3, 5, 5, 6],
+  ],
+  shoes: [
+    [1, 1, 3, 2],
+    [1, 2, 3, 3],
+    [1, 3, 3, 4],
+    [1, 4, 3, 5],
+    [1, 5, 3, 6],
+    [3, 1, 5, 2],
+    [3, 2, 5, 3],
+    [3, 3, 5, 4],
+    [3, 4, 5, 5],
+    [3, 5, 5, 6],
+  ],
+  bags: [
+    [1, 1, 3, 2],
+    [1, 2, 3, 3],
+    [1, 3, 3, 4],
+    [1, 4, 3, 5],
+    [1, 5, 3, 6],
+    [3, 1, 5, 2],
+    [3, 2, 5, 3],
+    [3, 3, 5, 4],
+    [3, 4, 5, 5],
+    [3, 5, 5, 6],
+  ],
+  accessories: [
+    [1, 1, 3, 2],
+    [1, 2, 3, 3],
+    [1, 3, 3, 4],
+    [1, 4, 3, 5],
+    [1, 5, 3, 6],
+    [3, 1, 5, 2],
+    [3, 2, 5, 3],
+    [3, 3, 5, 4],
+    [3, 4, 5, 5],
+    [3, 5, 5, 6],
+  ],
+};
+
 window.addEventListener("DOMContentLoaded", () => {
   universeGallery();
 });
 
+//init grid gallery functions
 const universeGallery = () => {
   const buttons = document.querySelectorAll(".universe-filter button");
+  const containerImg = document.querySelectorAll(".img-container");
 
+  createGrid(containerImg);
+  animFilter();
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
       const btn_data = btn.dataset.target;
-      console.log(btn_data);
-      currentView(btn_data);
+      animExitGallery();
+      setTimeout(() => {
+        currentView(btn_data);
+      }, 200);
     });
   });
 };
 
+// display / hide images selected
 const currentView = (btn_data) => {
-    
-const row  = document.querySelector('.universe-row')
-const containerImg = document.querySelectorAll(".img-container");
-
+  const containerImg = document.querySelectorAll(".img-container");
   containerImg.forEach((img) => {
     const img_data = img.dataset.category;
     if (btn_data === "all") {
@@ -31,26 +103,111 @@ const containerImg = document.querySelectorAll(".img-container");
       }
     }
   });
-  const newContainerImg =  Array.from(document.querySelectorAll('.img-container'));
-  console.log(newContainerImg)
+  createGrid(containerImg, btn_data);
+};
 
-  newContainerImg.sort((a,b) => {
-    const aHidden = a.classList.contains('hidden');
-    const bHidden = b.classList.contains('hidden');
-    if (aHidden && !bHidden) {
-        return 1;
-      } else if (!aHidden && bHidden) {
-        return -1;
-      } else {
-        return 0;
-      }
-  })
-  console.log(newContainerImg)
+//constructor grid gallery
+const createGrid = (container = [], btn_data = "all") => {
+  const ContainerVisible = document.querySelectorAll(".img-container:not(.hidden)");
+  const imgs = document.querySelectorAll(".img-container img");
 
-  containerImg.forEach((container) => {
-    row.removeChild(container);
+  let cpt = 0,
+    j = 0;
+
+  imgs.forEach((img) => {
+    img.style.aspectRatio = "1/1";
+    if (btn_data === "clothes") {
+      img.style.aspectRatio = "1/2";
+    }
   });
-  newContainerImg.forEach((container) => {
-    row.appendChild(container);
+
+  const currentArr = arrStyle[btn_data];
+  container.forEach((img, i) => {
+    img.style.gridArea = "none";
   });
+
+  ContainerVisible.forEach((img, i) => {
+    if (i % currentArr.length === 0 && i !== 0) {
+      cpt++;
+      j = 0;
+    }
+    img.style.gridArea = `${currentArr[j][0] + cpt * 4} / ${currentArr[j][1]} / ${currentArr[j][2] + cpt * 4} / ${
+      currentArr[j][3]
+    }`;
+    j++;
+  });
+
+  animEnterGallery();
+};
+
+
+// animations GSAP
+const animFilter = () => {
+  const filters = document.querySelectorAll(".universe-filter button");
+
+  const tl = gsap.timeline();
+  tl.fromTo(
+    filters,
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 1,
+      delay: 1,
+      stagger: 0.1,
+    }
+  );
+};
+
+const animExitGallery = () => {
+  const ContainerVisible = document.querySelectorAll(".img-container:not(.hidden)");
+
+  const tl = gsap.timeline();
+  tl.to(ContainerVisible, {
+    y: 30,
+    duration: 0.5,
+    stagger: 0.01,
+    opacity: 0,
+  });
+};
+
+const animEnterGallery = () => {
+  const ContainerVisible = document.querySelectorAll(".img-container:not(.hidden)");
+  const ImgVisible = document.querySelectorAll(".img-container:not(.hidden) img");
+
+  const tl = gsap.timeline();
+  const tl2 = gsap.timeline();
+
+  tl.fromTo(
+    ContainerVisible,
+    {
+      opacity: 0,
+      y: 20,
+      filter: "blur(30px)",
+    },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      delay: 0,
+      filter: "blur(0px)",
+      stagger: 0.07,
+      ease: "power4.in",
+    }
+  );
+  tl2.fromTo(
+    ImgVisible,
+    {
+      transform:"scale(1.3)"
+    },
+    {
+      ease: "power4.in",
+      transform:"scale(1.1)",
+      duration: 0.6,
+      delay: 0,
+      stagger: 0.07,
+      transformOrigin: "center center",
+    }
+  );
 };
