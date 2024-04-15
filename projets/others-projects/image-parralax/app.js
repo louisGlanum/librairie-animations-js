e13a3c9db3c8cf8ee6d235509208cda3abde0de0
Louis <louis@glanum.com>
2024-04-15 14:54:55 +0200

    feat(parralax-img): add new feature

---
+++ projets/others-projects/image-parralax/app.js
@@ -0,0 +1,32 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    parralaxImg('img', 0.3);
+})
+
+
+const parralaxImg = (target, speedDefault = 0.5, vertical = true) => {
+    const images = document.querySelectorAll(target)
+    window.addEventListener('scroll', () => {
+        window.requestAnimationFrame(() =>{
+            images.forEach((image) => {
+                const scrolled = window.scrollY
+                const speed = Number(image.dataset.speed) || speedDefault
+                if(vertical){
+                    image.style.transform = `translate3d(0, ${scrolled * -speed}px, 0)`
+                    image.style.height = `${100 + ((speed * 10) * 30)}%`
+                }else{
+                    image.style.transform = `translate3d(${scrolled * -speed}px, 0, 0)`
+                    image.style.width = `${100 + ((speed * 10) * 30)}%`
+                }
+            })
+        })
+    })
+}
+
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
