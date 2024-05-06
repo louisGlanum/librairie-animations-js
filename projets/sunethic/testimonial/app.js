bf18629fd8de38c582dc18042453159267396ae6
Louis <louis@glanum.com>
2024-05-06 17:01:43 +0200

    feat(sunethic): testimonial

--- projets/sunethic/testimonial/app.js
+++ projets/sunethic/testimonial/app.js
@@ -6,15 +6,15 @@ window.addEventListener('DOMContentLoaded', ()=> {
 const testimonialAnimation = () => {
     const section = document.querySelector('.testimonial_section');
     const image = section.querySelector('.testimonial_img');
-
-    const tl = gsap.timeline({
-        scrollTrigger:{
-            trigger: section,
-            start:"top bottom",
-            end:"bottom top",
-            scrub: true,
-            markers: true
-        }
-    })
-    tl.to(image, {scale:1})
+    if(section){
+        const tl = gsap.timeline({
+            scrollTrigger:{
+                trigger: section,
+                start:"top bottom",
+                end:"bottom top",
+                scrub: true,
+            }
+        })
+        tl.to(image, {scale:1})
+    }
 }
--- projets/sunethic/title/app.js
+++ projets/sunethic/title/app.js
@@ -1,26 +1,21 @@
+window.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+  animationScrollTitles();
+});
 
-
-window.addEventListener('DOMContentLoaded', () => {
-    gsap.registerPlugin(ScrollTrigger);
-    animationScrollTitles();
-    
-    })
-    
-    const animationScrollTitles = () => {
-        const wrappers = document.querySelectorAll('.title_wrapper');
-        
-        wrappers.forEach((wrapper) => {
-
-
-            const tl = gsap.timeline({
-                scrollTrigger:{
-                    trigger:wrapper,
-                    start:"top bottom",
-                    end:"top 30%",
-                    scrub:true,
-                    markers: true
-                }
-            })
-            tl.to(wrapper, {opacity:1})
-        })
-    }
+const animationScrollTitles = () => {
+  const wrappers = document.querySelectorAll(".title_wrapper");
+  if (wrappers.length) {
+    wrappers.forEach((wrapper) => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: wrapper,
+          start: "top bottom",
+          end: "top 30%",
+          scrub: true,
+        },
+      });
+      tl.to(wrapper, { opacity: 1 });
+    });
+  }
+};
