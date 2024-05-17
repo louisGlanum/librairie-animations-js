823260a64db65e57124fc92b1011239b1ba7490f
Louis <louis@glanum.com>
2024-05-17 15:42:38 +0200

    feat(pe25-paph): finish menu

--- projets/pe25-paph/menu/app.js
+++ projets/pe25-paph/menu/app.js
@@ -1,106 +1,171 @@
-window.addEventListener('DOMContentLoaded', () => {
-    introHome();
-    submenuPosition();
+window.addEventListener("DOMContentLoaded", () => {
+  toggleTheme();
 
+  introHome();
+  submenuPosition();
 
-    managePanel('.link-menu.look', '.submenu--look');
-    // managePanel('.link-menu.subclasses', '.submenu--subclasses');
-})
+  managePanel();
+  managePanelSide();
+
+});
 
 const introHome = () => {
-    const wrapper_menu = document.querySelector('.wrapper_menu');
-    const cmTop = wrapper_menu.querySelector('.cm--top');
-    const cmBottom = wrapper_menu.querySelector('.cm--bottom');
-    const cmLeft = wrapper_menu.querySelector('.cm--left');
-    const cmRight = wrapper_menu.querySelector('.cm--right');
+  const wrapper_menu = document.querySelector(".wrapper_menu");
+  const cmTop = wrapper_menu.querySelector(".cm--top");
+  const cmBottom = wrapper_menu.querySelector(".cm--bottom");
+  const cmLeft = wrapper_menu.querySelector(".cm--left");
+  const cmRight = wrapper_menu.querySelector(".cm--right");
 
-    const linkTop = cmTop.querySelectorAll('li .link-menu');
-    const linkBottom = cmBottom.querySelectorAll('li .link-menu');
-    const linkLeft = cmLeft.querySelectorAll('li .link-menu');
-    const linkRight = cmRight.querySelectorAll('li .link-menu');
-    const wrapper_global = document.querySelector('.wrapper_global');
+  const linkTop = cmTop.querySelectorAll("li .link-menu");
+  const linkBottom = cmBottom.querySelectorAll("li .link-menu");
+  const linkLeft = cmLeft.querySelectorAll("li .link-menu");
+  const linkRight = cmRight.querySelectorAll("li .link-menu");
+  const wrapper_global = document.querySelector(".wrapper_global");
 
-    const tl = gsap.timeline();
-    tl.to(cmTop, {y:0, duration:1, ease:"power3.inOut"})
-    .to(cmBottom, {y:0, duration:1, ease:"power3.inOut"},"<")
-    .to(cmLeft, {x:0, duration:1, ease:"power3.inOut"},"<")
-    .to(cmRight, {x:0, duration:1, ease:"power3.inOut"},"<")
-    .to(wrapper_global, {"--thickness-content":"40px", duration:1, ease:"power3.inOut"},"<")
-    .to(linkTop, {y:0, duration:1, ease:"power3.inOut"},"-=0.9")
-    .to(linkBottom, {y:0, duration:1, ease:"power3.inOut"},"<")
-    .to(linkLeft, {y:0, duration:1, ease:"power3.inOut"},"<")
-    .to(linkRight, {y:0, duration:1, ease:"power3.inOut"},"<")
-}
+  const tl = gsap.timeline();
+  tl.to(cmTop, { y: 0, duration: 1, ease: "power3.inOut" })
+    .to(cmBottom, { y: 0, duration: 1, ease: "power3.inOut" }, "<")
+    .to(cmLeft, { x: 0, duration: 1, ease: "power3.inOut" }, "<")
+    .to(cmRight, { x: 0, duration: 1, ease: "power3.inOut" }, "<")
+    .to(wrapper_global, { "--thickness-content": "40px", duration: 1, ease: "power3.inOut" }, "<")
+    .to(linkTop, { y: 0, duration: 1, ease: "power3.inOut" }, "-=0.9")
+    .to(linkBottom, { y: 0, duration: 1, ease: "power3.inOut" }, "<")
+    .to(linkLeft, { y: 0, duration: 1, ease: "power3.inOut" }, "<")
+    .to(linkRight, { y: 0, duration: 1, ease: "power3.inOut" }, "<");
+};
 
 const submenuPosition = () => {
-    const submenuLook = document.querySelector('.submenu--look');
-    const submenuSubclasses = document.querySelector('.submenu--subclasses')
-    const distanceLook = getPositionFromLeft('.look')
-    const distanceSubclasses = getPositionFromLeft('.subclasses');
+  const submenuLook = document.querySelector(".submenu--look");
+  const submenuSubclasses = document.querySelector(".submenu--subclasses");
+  const distanceLook = getPositionFromLeft(".look");
+  const distanceSubclasses = getPositionFromLeft(".subclasses");
 
-    submenuLook.style.marginLeft = `${distanceLook}`
-    submenuSubclasses.style.marginLeft = `${distanceSubclasses}`
+  submenuLook.style.marginLeft = `${distanceLook}`;
+  submenuSubclasses.style.marginLeft = `${distanceSubclasses}`;
 
-    window.addEventListener('resize' , () => {
-        const distanceLook = getPositionFromLeft('.look')
-        const distanceSubclasses = getPositionFromLeft('.subclasses');
-        submenuLook.style.marginLeft = `${distanceLook}`
-        submenuSubclasses.style.marginLeft = `${distanceSubclasses}`
-    })
-}
+  window.addEventListener("resize", () => {
+    const distanceLook = getPositionFromLeft(".look");
+    const distanceSubclasses = getPositionFromLeft(".subclasses");
+    submenuLook.style.marginLeft = `${distanceLook}`;
+    submenuSubclasses.style.marginLeft = `${distanceSubclasses}`;
+  });
+};
 
 const getPositionFromLeft = (className) => {
-    const element = document.querySelector(className);
-    const elementRect = element.getBoundingClientRect();
-    // 40 = --thickness-content
-    const distanceLeft = elementRect.left - 40;
-    return  `${distanceLeft}px`
-}
+  const element = document.querySelector(className);
+  const elementRect = element.getBoundingClientRect();
+  // 40 = --thickness-content
+  const distanceLeft = elementRect.left - 40;
+  return `${distanceLeft}px`;
+};
 
+const managePanelSide = () => {
+  const panel = document.querySelector(".panel-side");
+  const link = document.querySelector(".link-menu-right");
+  const data = link.dataset.panel;
+  const submenu = document.querySelector(`.submenu--${data}`);
+
+  const tl = gsap.timeline({ paused: true });
+  tl.to(submenu, { opacity: 1, duration: 0.5, delay: 0.2 });
+
+  link.submenu = submenu;
+  link.panel = panel;
+  link.tl = tl;
+  panel.tl = tl;
+
+  link.addEventListener("mouseenter", openSidePanel);
+  panel.addEventListener("mouseleave", closeSidePanel);
+
+  function openSidePanel() {
+    const theme = document.body.classList.contains('dark');
+    panel.style.width = `300px`;
+    panel.style.borderLeft =  theme ? `1px solid white` : `1px solid #292c35`;
+    this.panel.style.pointerEvents = "all";
+    this.tl.play();
+    setTimeout(() => {
+      panel.style.borderLeft =  theme ? `1px solid white` : `1px solid #292c35`;
+    }, 300);
+  }
+  function closeSidePanel() {
+    const theme = document.body.classList.contains('dark');
+    this.style.width = `0px`;
+
+    this.style.pointerEvents = "none";
+    this.tl.restart().pause();
+    setTimeout(() => {
+      panel.style.borderLeft = theme ? `1px solid white` : `1px solid #292c35`;
+    }, 300);
+  }
+};
 
 const managePanel = () => {
-    const panel = document.querySelector('.panel');
-    const linksMenu = document.querySelectorAll('.link-menu-top');
+  const panel = document.querySelector(".panel");
+  const linksMenu = document.querySelectorAll(".link-menu-top");
 
-    linksMenu.forEach((link) =>{
-       const data = link.dataset.panel;
-       const submenu = document.querySelector(`.submenu--${data}`)
-       const tl = gsap.timeline({paused:true});
-       tl.to(submenu, {opacity:1, duration:1, delay:0.2})
-        
+  linksMenu.forEach((link) => {
+    const data = link.dataset.panel;
+    const submenu = document.querySelector(`.submenu--${data}`);
+    const tl = gsap.timeline({ paused: true });
+    tl.to(submenu, { opacity: 1, duration: 0.5, delay: 0.2 });
 
-        link.submenu = submenu;
-        link.panel = panel;
-        link.tl = tl;
-        submenu.panel = panel;
-        submenu.tl = tl;
-        
-        link.addEventListener('mouseenter', openPanel);
-        submenu.addEventListener('mouseleave', closePanel);
-    })
+    link.submenu = submenu;
+    link.panel = panel;
+    link.tl = tl;
+
+
+    submenu.panel = panel;
+    submenu.tl = tl;
+
+
+    link.addEventListener("mouseenter", openPanel);
+    submenu.addEventListener("mouseleave", closePanel);
+  });
+
+  function openPanel() {
+    const theme = document.body.classList.contains('dark');
+    console.log(theme);
+    linksMenu.forEach((link) => {
+      link.tl.restart().pause();
+      link.submenu.style.pointerEvents = "none";
+    });
+    const height = this.submenu.offsetHeight;
+    panel.style.height = `${height}px`;
+    panel.style.borderBottom = theme ? `1px solid white` : `1px solid #292c35`;
+    this.submenu.style.pointerEvents = "all";
+    this.tl.play();
+    setTimeout(() => {
+      panel.style.borderBottom = theme ? `1px solid white` : `1px solid #292c35`;
+    }, 300);
+  }
+
+  function closePanel() {
+    const theme = document.body.classList.contains('dark');
+    panel.style.height = `0px`;
+    this.style.pointerEvents = "none";
+    this.tl.restart().pause();
+    setTimeout(() => {
     
-    function openPanel(){
-        linksMenu.forEach((link) => {
-            link.tl.restart().pause()
-            link.submenu.style.pointerEvents ='none';
-        });
-        const height = this.submenu.offsetHeight;
-        panel.style.height = `${height}px`;
-        panel.style.borderBottom = `1px solid black`
-        this.submenu.style.pointerEvents = 'all';
-        this.tl.play();
-        setTimeout(() => {
-            panel.style.borderBottom = `1px solid black`
-        }, 300)
-    }
-    
-    function closePanel(){
-        panel.style.height = `0px`;
+      panel.style.borderBottom = theme ? `0px solid white` : `0px solid #292c35`;
+    }, 300);
+  }
+};
 
-        this.style.pointerEvents = 'none';
-        this.tl.restart().pause();
-        setTimeout(() => {
-            panel.style.borderBottom = `0px solid black`
-        }, 300)
+const toggleTheme = () => {
+  const checkbox = document.getElementById("checkbox");
+
+  if (!localStorage.getItem("dark")) {
+    localStorage.setItem("dark", JSON.stringify(false));
+  } else {
+    console.log("localStorage déja set");
+
+    if (JSON.parse(localStorage.getItem("dark")) === true) {
+      document.body.classList.add("dark");
     }
+  }
+
+  checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+    const switchTheme = !JSON.parse(localStorage.getItem("dark"));
+    localStorage.setItem("dark", JSON.stringify(switchTheme));
+  });
 };
